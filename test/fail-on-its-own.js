e7906cf176563f48b9d1adc7afad4d8d3dd4026f
Jessica Jordan <jessica.jordan.fe.dev@gmail.com>
2019-09-04 15:23:19 +0200

    feat: reproduction of error in https://github.com/qunitjs/qunit/issues/1405

---
+++ test/fail-on-its-own.js
@@ -0,0 +1,3 @@
+QUnit.test( "I might pass when run with another test, but will fail on my own", function( assert ) {
+  assert.expect(0);
+});
---
+++ test/pass.js
@@ -0,0 +1,3 @@
+QUnit.test( "I pass", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
+});
